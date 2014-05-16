7b8664b7d71ce3c977ac0d2671610b3e4a199041
Carlos Espejo <carlosespejo@gmail.com>
2014-05-16 12:23:26 -0400

    api change to enable reading the template document once and generating many word documents with different inputs

--- lib/open_xml/template_document.rb
+++ lib/open_xml/template_document.rb
@@ -3,11 +3,10 @@ require 'nokogiri'
 
 module OpenXml
   class TemplateDocument
-    attr_reader :template_path, :parts, :data
+    attr_reader :template_path, :parts
 
     def initialize(options)
       @template_path = options.fetch(:path)
-      @data = options[:data]
       @parts = {}
       split_parts
     end
@@ -21,7 +20,9 @@ module OpenXml
       end
     end
 
-    def process
+    def process(data)
+      @parts = @parts_cache.clone
+
       doc = Nokogiri::XML(parts['word/document.xml'])
       doc.xpath('//w:t').each do |node|
         data.each do |key, value|
@@ -34,7 +35,7 @@ module OpenXml
                 br = Nokogiri::XML::Node.new 'w:br', doc
                 n = Nokogiri::XML::Node.new 'w:t', doc
 
-                n.content = v
+                n.content = v.to_s
 
                 node.parent << n
                 node.parent << br
@@ -42,7 +43,7 @@ module OpenXml
 
               node.remove
             else
-              node.content = node.content.gsub(key, values.first) if values.first
+              node.content = node.content.gsub(key, values.first.to_s) if values.first
             end
 
           end
@@ -60,7 +61,7 @@ module OpenXml
         parts[f.name] = f.get_input_stream.read
       end
 
-      parts
+      @parts_cache = parts.clone
     end
   end
 end
--- spec/template_document_spec.rb
+++ spec/template_document_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 require 'tempfile'
 require 'nokogiri'
+require 'pry'
 
 describe TemplateDocument do
 
   it 'should read the template file and split it into parts' do
     t = TemplateDocument.new(path: template_path)
-    t.parts.keys.must_equal template_parts.keys
-
+    t.parts.keys.must_include 'word/document.xml'
   end
 
   it 'should create a new file from the template parts' do
@@ -24,22 +24,18 @@ describe TemplateDocument do
   end
 
   it 'should replace key words in the document xml' do
-    temp = Tempfile.new(['output', '.docx'])
+    t = TemplateDocument.new(path: template_path)
+    t.process({ 'person_name' => '<b>Carlos</b>', 'person_age' => 30 })
+    doc = Nokogiri::XML(t.parts['word/document.xml'])
 
-    t = TemplateDocument.new(path: template_path, data: { '[NAME]' => '<b>Carlos</b>', '[AGE]' => 30 })
-    t.process
-    IO.write temp.path, t.to_zip_buffer.string
-
-    processed = TemplateDocument.new(path: temp.path)
-    doc = Nokogiri::XML(processed.parts['word/document.xml'])
-
-    doc.xpath('//w:t').text[/\[NAME\]/].must_be_nil
-    doc.xpath('//w:t').text[/\[AGE\]/].must_be_nil
+    text = doc.xpath('//w:t').text
+    text[/Carlos/].wont_be_nil
+    text[/30/].wont_be_nil
   end
 
   it 'should replace one key word with many items' do
     data = {
-      '[LIST]' => [
+      'my_list' => [
                     'list 1',
                     'list 2',
                     'list 3',
@@ -48,32 +44,31 @@ describe TemplateDocument do
                   ]
     }
 
-    t = TemplateDocument.new(path: template_path, data: data)
-    t.process
+    t = TemplateDocument.new(path: template_path)
+    t.process(data)
+
     doc = Nokogiri::XML(t.parts['word/document.xml'])
 
     text = doc.xpath('//w:t').text
 
-    text[/list 1\nlist 2/].wont_be_nil
+    text[/list 2/].wont_be_nil
+  end
+
+  it "should cache the template document" do
+    t = TemplateDocument.new(path: template_path)
+    t.process({'person_name' => 'steve'})
+    doc = Nokogiri::XML(t.parts['word/document.xml'])
+
+    doc.text[/steve/].wont_be_nil
+
+    t.process({'person_name' => 'carlos'})
+    doc = Nokogiri::XML(t.parts['word/document.xml'])
+
+    doc.text[/carlos/].wont_be_nil
   end
 
   def template_path
     "#{File.expand_path('samples', File.dirname(__FILE__))}/template_sample.docx"
   end
 
-  def template_parts
-    {
-      '_rels/.rels' => '',
-      'docProps/core.xml' => '',
-      'docProps/app.xml' => '',
-      'word/document.xml' => '',
-      'word/styles.xml' => '',
-      'word/fontTable.xml' => '',
-      'word/header.xml' => '',
-      'word/footer.xml' => '',
-      'word/settings.xml' => '',
-      'word/_rels/document.xml.rels' => '',
-      '[Content_Types].xml' => ''
-    }
-  end
 end
